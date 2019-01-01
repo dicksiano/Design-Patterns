dce51db6d0bc101964846ba88708a4dbf5e59a2d
dicksiano <dicksianomelo@gmail.com>
2018-12-31 23:37:32 -0300

    Bridge

---
+++ Structural Patterns/Bridge.java
@@ -0,0 +1,28 @@
+class A {
+    private ImplA _implA;
+
+}
+
+class FirstA extends A {
+
+}
+
+class SecondA extends A {
+
+}
+
+interface ImplA {
+
+}
+
+class FirstImplA implements ImplA {
+
+}
+
+class SecondImplA implements ImplA {
+    
+}
+
+public class Bridge {
+
+}
